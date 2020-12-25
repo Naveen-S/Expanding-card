8aa63f3c5a71f89911e1c95b3a81e0bea6bcff6a
naveen <naveentejas1109@gmail.com>
2020-12-25 20:56:47 +0530

    unnecessary imports and logs

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
-console.log('Linked');
-
 const panels = document.querySelectorAll('.panel');
 
 panels.forEach((panel) => {
   panel.addEventListener('click', (e) => {
-    console.log(e);
     removeActiveClasses();
     panel.classList.add('active');
   });
